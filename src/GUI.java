7fe41c090010b5686ca766c31e58102569374c69
caity k <cdk6546@rit.edu>
2023-02-25 14:46:30 -0500

    Initial Commit

---
+++ src/GUI.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.jar.JarEntry;
+
+public class GUI extends JFrame {
+    private JButton button1;
+    private JPanel panel1;
+
+    private static ImageIcon img;
+
+    private static JLabel theimg;
+
+    public GUI(){
+        JFrame k = new JFrame();
+        theimg = new JLabel(new ImageIcon("src/2.png"));
+
+
+        k.setUndecorated(true);
+        k.setBackground(new Color(0, 0, 0, 0));
+        k.setLayout(null);
+
+
+        theimg.setBounds(1200, 690, 200, 200);
+
+        k.add(theimg);
+        theimg.setVisible(true);
+        k.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        k.setAlwaysOnTop(true);
+        k.setVisible(true);
+
+
+    }
+
+    public static void main(String[] args){
+        GUI h = new GUI();
+
+    }
+}
